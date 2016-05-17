935969d29f0f8cefa53667ae8c6e31eb39ec65c2
Jude Cooray and Rui Baltazar <pair+jude+rui@opsmanager.com>
2016-05-17 09:55:54 +0800

    Revert "WIP - moved rainbow mode to init filetypes"
    This reverts commit a14f31472a652f25f2fe4ba57b4e3db55bfe252d.

--- config/init-filetypes.el
+++ config/init-filetypes.el
@@ -1,6 +1,5 @@
 ;; setup files ending in “.hamlc” to open in haml
 (add-to-list 'auto-mode-alist '("\\.hamlc\\'" . haml-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . sass-mode))
-(add-to-list 'auto-mode-alist '("\\.scss\\'". rainbow-mode ))
 
 (provide 'init-filetypes)
