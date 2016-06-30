c9dab7c3013ac6fd83aeff7915d12e7a6812f397
Rui Baltazar <rui.p.baltazar@gmail.com>
2016-06-30 14:37:49 +0800

    put bell back as it was causing problems and fixed sass mode

--- config/init-rainbow.el
+++ config/init-rainbow.el
@@ -1,2 +1,4 @@
-(add-to-list 'auto-mode-alist '("\\.scss\\'". rainbow-mode ))
+(add-to-list 'auto-mode-alist '("\\.scss\\'". sass-mode ))
+(add-hook 'sass-mode-hook 'rainbow-mode)
+
 (provide 'init-rainbow)
