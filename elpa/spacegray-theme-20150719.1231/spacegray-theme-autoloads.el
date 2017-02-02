af48754ec0e45dda6a4f53b0ac32a7e009e2e55d
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-02-02 14:15:47 +0800

    updated packages

--- elpa/spacegray-theme-20150719.1231/spacegray-theme-autoloads.el
+++ elpa/spacegray-theme-20150719.1231/spacegray-theme-autoloads.el
@@ -1,13 +1,15 @@
 ;;; spacegray-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (22387
-;;;;;;  11826 0 0))
+;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (0 0
+;;;;;;  0 0))
 ;;; Generated autoloads from spacegray-theme.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spacegray-theme" '("spacegray")))
 
 ;;;***
 
